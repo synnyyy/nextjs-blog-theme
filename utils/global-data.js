5f416070ae53a60c7b571f4a71ca86257ac359c5
synnyyy <121168410+synnyyy@users.noreply.github.com>
2023-06-30 22:11:20 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'For exploiters, by exploiters.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'ALL RIGHTS RESERVED.';
+    : 'discord.gg/Rrag82qTfw';
 
   return {
     name,
