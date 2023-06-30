da428ac6b9a00f79fb83dce4adbe30807d7cabfc
synnyyy <121168410+synnyyy@users.noreply.github.com>
2023-06-30 22:21:10 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -14,46 +14,5 @@ export function GradientBackground({ variant, className }) {
   return <div className={classes} />;
 }
 
-export default function Layout({ children }) {
-  const setAppTheme = () => {
-    const darkMode = localStorage.getItem('theme') === 'dark';
-    const lightMode = localStorage.getItem('theme') === 'light';
-
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-    } else if (lightMode) {
-      document.documentElement.classList.remove('dark');
-    }
-    return;
-  };
-
-  const handleSystemThemeChange = () => {
-    var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-    darkQuery.onchange = (e) => {
-      if (e.matches) {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('theme', 'dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('theme', 'light');
-      }
-    };
-  };
-
-  useEffect(() => {
-    setAppTheme();
-  }, []);
-
-  useEffect(() => {
-    handleSystemThemeChange();
-  }, []);
-
-  return (
-    <div className="relative pb-24 overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
-        {children}
-      </div>
-    </div>
-  );
-}
+document.documentElement.classList.add('dark');
+localStorage.setItem('theme', 'dark');
